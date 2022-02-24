70754052b57ea24bc36dd27380d86eb1a824b533
yerramsettysaimeghana <saimeghanayerramsetty@gmail.com>
2022-02-24 17:18:35 +0530

    logo and heading

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import { Typography, Box } from "@material-ui/core";
+
+function About() {
+  return (
+    <Box bgcolor="black">
+  <Typography style={{color:"white"}}>About us page</Typography>
+  </Box>
+  );
+}
+export default About;
